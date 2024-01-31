e43a1c33e1afcbb26bd67491e5596fda1a2e17bc
Ganesh-P03 <84033688+Ganesh-P03@users.noreply.github.com>
2024-02-01 01:13:37 +0530

    Added NFT minting

--- smartcontract/contracts/MarketPlace.sol
+++ smartcontract/contracts/MarketPlace.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MarketPlace is Ownable {
+contract MarketPlace is Ownable, ERC721URIStorage {
     // Product
     struct Product {
         string name;
@@ -15,11 +17,12 @@ contract MarketPlace is Ownable {
 
     Product[] private products;
 
-    // Timestamps of sold,Address of customer
+    // Timestamps of sold, Address of customer
     string public marketPlaceName; 
     string public marketPlaceDescription;
+    uint private tokenId = 0;
 
-    constructor(string memory _name, string memory _description) Ownable(msg.sender) {
+    constructor(string memory _name, string memory _description) Ownable(msg.sender) ERC721("MarketplaceToken", "MPT") {
         marketPlaceName = _name;
         marketPlaceDescription = _description;
     }
@@ -27,8 +30,9 @@ contract MarketPlace is Ownable {
     event ProductAdded(string name, uint price, uint quantity, string ipfsLink);
     event DeleteProduct(string name);
     event EditProduct(string name, uint price, uint quantity, string ipfsLink);
+    event ProductPurchased(uint indexed tokenId, uint productId, address buyer);
 
-    function addProduct(string memory _name, string memory _description,uint _price, uint _quantity, string memory _ipfsLink) public {
+    function addProduct(string memory _name, string memory _description, uint _price, uint _quantity, string memory _ipfsLink) public {
         require(bytes(_name).length > 0, "Name required");
         require(_price > 0, "Price required");
         require(bytes(_description).length > 0, "Description required");
@@ -36,7 +40,7 @@ contract MarketPlace is Ownable {
         require(bytes(_ipfsLink).length > 0, "IPFS Link required");
 
         products.push(Product(_name, _description, _price, _quantity, _ipfsLink, 0));
-        emit ProductAdded( _name, _price, _quantity, _ipfsLink);
+        emit ProductAdded(_name, _price, _quantity, _ipfsLink);
     }
 
     function getProducts() public view returns (Product[] memory) {
@@ -54,7 +58,7 @@ contract MarketPlace is Ownable {
         products.pop();
     }
 
-    function editProduct(uint _id, string memory _name, string memory _description,uint _price, uint _quantity, string memory _ipfsLink) public {
+    function editProduct(uint _id, string memory _name, string memory _description, uint _price, uint _quantity, string memory _ipfsLink) public {
         require(_id >= 0 && _id < products.length, "Invalid product id");
         require(bytes(_name).length > 0, "Name required");
         require(bytes(_description).length > 0, "Description required");
@@ -71,20 +75,29 @@ contract MarketPlace is Ownable {
         emit EditProduct(_name, _price, _quantity, _ipfsLink);
     }
 
-    function buyProduct(uint _id) public payable  {
+    function buyProduct(uint _id) public payable {
         require(_id >= 0 && _id < products.length, "Invalid product id");
         require(products[_id].quantity > 0, "Product out of stock");
-        require( msg.value >= products[_id].price, "Insufficient funds");
+        require(msg.value >= products[_id].price, "Insufficient funds");
 
         // Transfer funds to the contract owner
         address payable owner = payable(owner());
         owner.transfer(products[_id].price);
 
+        // Set the token URI with product details
+        string memory tokenUri = products[_id].name;
+        _setTokenURI(tokenId, tokenUri);
+
+        // Transfer the token to the buyer
+        _safeMint(msg.sender, tokenId);
+
         // Update product information
         products[_id].sold += 1;
+        products[_id].quantity -= 1;
+        tokenId += 1;
+
+        // Emit an event for the purchase
+        emit ProductPurchased(tokenId, _id, msg.sender);
     }
+
 }
-
-
-
-
